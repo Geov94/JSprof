cc525b61577f4311219b0b45eda55f16f1330f9b
Geov94 <agsalamanca@misena.edu.co>
2022-02-02 01:09:48 -0500

    implementando serviceworker,corregir errores

--- assets/index.js
+++ assets/index.js
@@ -10,7 +10,12 @@ button.onclick =() => player.play();
 buttonUnmute.onclick = () =>player.mutedToggle();
 
 
+if ('serviceWorker' in navigator){
 
+    navigator.serviceWorker.register('/sw.js').catch(error =>{
+        console.log(error.message)
+    })
+}
 
  
  
