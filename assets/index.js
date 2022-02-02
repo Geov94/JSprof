dd582a4aec14e05d7f9772535c675215024d6ac7
Geov94 <agsalamanca@misena.edu.co>
2022-02-02 15:30:18 -0500

    cambios

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,9 @@ import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 const buttonUnmute = document.getElementById("buttonUnmute");
-const player = new MediaPlayer({ el: video,plugins:[new AutoPlay()]});
+const player = new MediaPlayer({ el: video,plugins:[
+    //new AutoPlay()
+]});
 button.onclick =() => player.play();
 buttonUnmute.onclick = () =>player.mutedToggle();
 
