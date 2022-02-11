5048932b8060a75e06f0f3ee92cabc3f0011be5a
Geov94 <agsalamanca@misena.edu.co>
2022-02-10 21:13:47 -0500

    importando ads problema con el Ad al importar

--- assets/index.ts
+++ assets/index.ts
@@ -1,11 +1,12 @@
 import MediaPlayer from "./mediaPlayer";
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause'
+import Ads from "./plugins/ads";
 
 const video:HTMLElement = document.querySelector("video");
 const button:HTMLElement  = document.querySelector("button");
 const buttonUnmute:HTMLElement = document.getElementById("buttonUnmute");
-const player = new MediaPlayer({ el: video,plugins:[new AutoPlay(),new AutoPause()]});
+const player = new MediaPlayer({ el: video,plugins:[new AutoPlay(),new AutoPause(),new Ads()]});
 button.onclick =() => player.play();
 buttonUnmute.onclick = () =>player.mutedToggle();
 
